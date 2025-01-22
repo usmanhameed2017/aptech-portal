c789ffcf1f1a1d21938be030174b13e1550a071a
Usman Hameed <85105769+Usman-Gray-Hat@users.noreply.github.com>
2025-01-22 13:11:21 -0800

    Fee Management Software For Aptech URC

--- Database/aptechurc_db.sql
+++ Database/aptechurc_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 30, 2024 at 04:17 AM
+-- Generation Time: Jan 21, 2025 at 11:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
